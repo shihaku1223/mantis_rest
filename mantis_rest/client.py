5136730ae07f3a5971f08336879240d261da5b7e
王　詩博 <sibo_wang@ot.olympus.co.jp>
2021-01-13 13:44:18 +0900

    add REST API definition, using get_obj and create_obj perform request
    new API definition
    get_issue_files, get_issue_file, get_issues_project
    create_issue, get_all_projects, get_all_filters
    get_filter, user_info

--- mantis_rest/client.py
+++ mantis_rest/client.py
@@ -6,6 +6,10 @@ import json
 
 rest_api_list = rest_api.get_api_list()
 
+class obj:
+    def __init__(self, dict1):
+        self.__dict__.update(dict1)
+
 class Client:
 
     def __init__(self, url, token):
@@ -14,19 +18,65 @@ class Client:
         self.headers = {'Authorization': self.token}
 
         for api_name in rest_api_list:
-            print(api_name)
-            impl = getattr(rest_api, '__{}_impl'.format(api_name))
-            setattr(Client, api_name, impl)
 
-    def request(self, url):
-        r = requests.get(url, headers=self.headers)
+            impl_name = '__{}_impl'.format(api_name)
+            try:
+                impl = getattr(rest_api, impl_name)
+                setattr(Client, api_name, impl)
+            except AttributeError:
+                impl = self.generate_api_impl(impl_name)
+                setattr(Client, api_name, impl)
+
+    def generate_api_impl(self, name):
+
+        # default api implementation
+        def impl(client, data):
+            pass
+
+        impl.__name__ = name
+        return impl
+
+    def request(self, url, data=None, headers=None):
+        if headers is None:
+            headers = self.headers
+        else:
+            headers = {**self.headers, **headers}
+
+        if data is None:
+            r = requests.get(url, headers=headers)
+        else:
+            print(url, data, headers)
+            r = requests.post(url, headers=headers, data=data)
+
         return r.text
 
-
 def get_obj(api):
-    def wrap(params):
-        r = api(params)
-        return json.loads(r)
+    def wrap(params=None):
+        path = rest_api.fill_api_path(
+                    rest_api.get_api_path(api.__name__), params)
+
+        url = urllib.parse.urljoin(client.url, path)
+        r = client.request(url)
+
+        if r is None or r == "":
+            return None
+        return json.loads(r, object_hook=obj)
+    return wrap
+
+def create_obj(api):
+    def wrap(params=None):
+        path = rest_api.fill_api_path(
+                    rest_api.get_api_path(api.__name__), params)
+
+        url = urllib.parse.urljoin(client.url, path)
+
+        h = { 'Content-Type': 'application/json' }
+        data = json.dumps(params)
+        r = client.request(url, headers=h, data=data)
+
+        if r is None or r == "":
+            return None
+        return json.loads(r, object_hook=obj)
     return wrap
 
 if __name__ == '__main__':
@@ -34,5 +84,29 @@ if __name__ == '__main__':
 
     client = Client('https://enosta.olympus.co.jp/mantis/ipf3/app/',
             'token')
-    result = get_obj(client.get_issue)({ ':issue_id': '1234' })
-    print(result)
+    """
+    get_obj(client.get_issue)({ ':issue_id': '1234' })
+    get_obj(client.get_issue_files)({ ':issue_id': '1234' })
+    get_obj(client.get_all_issues)({ ':page_size': '50', ':page': '1' })
+    get_obj(client.get_issues_project)({ ':project_id': '1' })
+    get_obj(client.get_all_projects)()
+    get_obj(client.get_filter)({ ':filter_id': '11092' })
+    get_obj(client.user_info)()
+    projects = get_obj(client.get_all_projects)().projects
+    for p in projects:
+        print(p.id, p.name, p.status.name, p.description)
+    """
+
+    data = {
+        "summary": "This is a test issue",
+        "description": "This is a test description",
+        "category": {
+            "name": "General"
+        },
+        "project": {
+            "name": "TEST"
+        }
+    }
+
+    issue_obj = create_obj(client.create_issue)(data)
+    print(issue_obj.issue.id)
--- mantis_rest/rest_api.py
+++ mantis_rest/rest_api.py
@@ -6,7 +6,24 @@ import re
 
 from types import ModuleType
 
+# issue API
 get_issue = 'api/rest/issues/:issue_id'
+get_issue_files = 'api/rest/issues/:issue_id/files'
+get_issue_file = 'api/rest/issues/:issue_id/files/:file_id'
+get_all_issues = 'api/rest/issues?page_size=:page_size&page=:page'
+get_issues_project = 'api/rest/issues?project_id=:project_id'
+create_issue = 'api/rest/issues/'
+
+# project API
+get_all_projects = 'api/rest/projects'
+
+# filter API
+get_all_filters = 'api/rest/filters'
+get_filter = 'api/rest/filters/:filter_id'
+# delete_filter = 'api/rest/filters/:filter_id'
+
+# user API
+user_info = 'api/rest/users/me'
 
 def get_api_list():
     rest_api_list = \
@@ -18,21 +35,27 @@ def get_api_list():
         ]
     return rest_api_list
 
-def get_api_path(client):
+def get_api_path(co_name):
+
+    """
     frames = inspect.getouterframes(inspect.currentframe())
     co_name = frames[1].frame.f_code.co_name[2:]
+    s = len(co_name) - 5
+
+    api_name = co_name[:s]
+    print(sys.modules[__name__])
+    """
+    co_name = co_name[2:]
     s = len(co_name) - 5
 
     api_name = co_name[:s]
     return getattr(sys.modules[__name__], api_name)
 
 def fill_api_path(path, params):
+    if params is None:
+        return path
+
     rep = dict((re.escape(k), v) for k, v in params.items())
     pattern = re.compile("|".join(rep.keys()))
 
     return pattern.sub(lambda m: rep[re.escape(m.group(0))], path)
-
-def __get_issue_impl(client, data):
-    path = fill_api_path(get_api_path(client), data)
-    url = urllib.parse.urljoin(client.url, path)
-    return client.request(url)
