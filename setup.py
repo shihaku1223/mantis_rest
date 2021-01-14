f2399f993a0f32a86b4c45f1a8b83a5375b91dcd
王　詩博 <sibo_wang@ot.olympus.co.jp>
2021-01-14 15:25:48 +0900

    add deployment script

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_requires = [
+    'requests',
+]
+
+setuptools.setup(
+    name="mantis_rest",
+    version="0.0.1",
+    author="Shih-Po Wang",
+    author_email="shihpo@gmail.com",
+    description="The Python binding Mantis REST API",
+    install_requires=install_requires,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
