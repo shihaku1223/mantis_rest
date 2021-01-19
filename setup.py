a24bf93880dc7b7f2768c70e4faf6fff90f3b173
王　詩博 <sibo_wang@ot.olympus.co.jp>
2021-01-19 09:23:12 +0900

    update version to 0.0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = [
 
 setuptools.setup(
     name="mantis_rest",
-    version="0.0.1",
+    version="0.0.4",
     author="Shih-Po Wang",
     author_email="shihpo@gmail.com",
     description="The Python binding Mantis REST API",
